567a47508d095d8e194be35f0b8d8a40e0791961
Jason Coon <3598755+jasoncoon@users.noreply.github.com>
2022-10-08 10:03:09 -0500

    Add new 'palette noise' and 'coronal mass ejection' patterns. Add header back to colorwaves.js

--- F64 HDR Micro/sources/palette noise.js
+++ F64 HDR Micro/sources/palette noise.js
@@ -1,7 +1,3 @@
-// Color Waves pattern
-// based on ColorWavesWithPalettes by Mark Kriegsman: https://gist.github.com/kriegsman/8281905786e8b2632aeb
-// modified by Ben Hencke and Jason Coon to run on Pixelblaze
-
 var autoPalette = true;
 var secondsPerPalette = 10;
 var paletteIndex = 0;
@@ -23,86 +19,62 @@ export function showNumberPalette() {
   return paletteIndex;
 }
 
+var autoMode = true;
+var secondsPerMode = 3;
+var modeIndex = 0;
+
+export function toggleAutoMode(v) {
+  autoMode = v;
+}
+
+export function inputNumberSecondsPerMode(v) {
+  secondsPerMode = v;
+}
+
+export function sliderMode(v) {
+  modeIndex = floor(v * 3)
+  modeFn = modes[modeIndex];
+}
+
+export function showNumberMode() {
+  return modeIndex;
+}
+
+modes = [
+    (x, y, z) => (perlin(x, y, z, 0) + 1) / 2,
+    (x, y, z) => perlinRidge(x, y, z, 2, .5, 1.1, 3),
+    (x, y, z) => (perlinFbm(x, y, z, 2, .5, 3) + 1) / 2,
+    (x, y, z) => perlinTurbulence(x, y, z, 2, .5, 3),
+]
+var modeFn;
+
+var wrap = 2;
+setPerlinWrap(wrap, wrap, wrap); 
+
 export function beforeRender(delta) {
+  // per-frame animation timers
+  t1 = time(.1);
+  noiseTime = time(7) * 256;
+  
   if (autoPalette) {
     paletteIndex = time(secondsPerPalette) * palettes.length;
     setPalette(palettes[paletteIndex]);
   }
   
-  colorwaves(delta, 1)
-}
-
-export function render(index) {
-  h = ledarray[index*3]
-  v = ledarray[index*3+2]
-  paint(h, v * v);
-}
-
-// beatsin8( BPM, uint8_t low, uint8_t high) returns an 8-bit value that
-// rises and falls in a sine wave, 'BPM' times per minute,
-// between the values of 'low' and 'high'.
-function beatsin8(bpm, low, high) {
-  return wave(time(0.91552734375/bpm)) * (high - low) + low
-}
-
-function beatsin88(bpm, low, high) {
-  return beatsin8(bpm>>8, low, high);
-}
-
-var sPseudotime = 0; //was uint16_t modified to be a value between 0 and 1
-// var sLastMillis = 0; //uint16_t
-var sHue16 = 0; //was uint16_t seems to work fine as-is
-var ledarray = array(pixelCount*3);
-
-function colorwaves(deltams, useFibonacciOrder) {
-  // var sat8 = beatsin88( 87, 220, 250); //uint8_t
-  // var brightdepth = beatsin88( 341, 96, 224); //uint8_t
-  var brightdepth = beatsin88(171, 96, 224); //uint8_t
-  // var brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256)); //uint16_t
-  var brightnessthetainc16 = beatsin88( 102, (25 * 256), (40 * 256)); //uint16_t
-  // var msmultiplier = beatsin88(147, 23, 60); //uint8_t
-  var msmultiplier = beatsin88(74, 23, 60); //uint8_t
-
-  var hue16 = sHue16;//gHue * 256; //uint16_t
-  // var hueinc16 = beatsin88(113, 300, 1500); //uint16_t
-  // var hueinc16 = beatsin88(57, 1, 128); //uint16_t
-  var hueinc16 = beatsin88(57, 1, 128*3); //varies a bit more
-
-  // var ms = millis(); //uint16_t
-  // var deltams = ms - sLastMillis ; //uint16_t
-  // sLastMillis  = ms;
-  sPseudotime += (deltams * msmultiplier) >>16;
-  // sHue16 += deltams * beatsin88( 400, 5, 9);
-  sHue16 += deltams * beatsin88( 200, 5, 9);
-  var brightnesstheta16 = sPseudotime; //uint16_t
-
-  for ( var i = 0 ; i < pixelCount; i++) { //uint16_t
-    hue16 += hueinc16;
-    var hue8 = triangle(hue16 >>16) * 128
-
-    brightnesstheta16  += brightnessthetainc16>>16;
-    brightnesstheta16 = mod(brightnesstheta16 + (brightnessthetainc16>>16), 1)
-    var b16 = wave( brightnesstheta16); //uint16_t
-
-    //var bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536; //uint16_t
-    var bri16 = b16 * b16
-    //var bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536; //uint8_t
-    var bri8 = bri16 * (brightdepth>>8)
-    bri8 += (1 - (brightdepth>>8));
-
-    var index = hue8; //uint8_t
-    //index = triwave8( index);
-    index = index/256 * 240;
-
-    // CRGB newcolor = ColorFromPalette( palette, index, bri8);
-
-    var pixelnumber = useFibonacciOrder ? fibonacciToPhysical[i] : i; //uint16_t
-
-    // nblend( ledarray[pixelnumber], newcolor, 8);
-    //TODO palletes, blending in RGB. For now use the 3 byte pixel for hue and value
-    ledarray[pixelnumber*3] = hue8 / 128;
-    ledarray[pixelnumber*3 + 2] = bri8;
+  if (autoMode) {
+    modeIndex = time(secondsPerMode) * 4;
+    modeFn = modes[modeIndex];
   }
+}
+
+export function render2D(index, x, y) {
+  radius = hypot(x - .5, y - .5) * 2 - noiseTime / 2;
+  radians = atan2(y - .5, x - .5);
+  angle = radians / PI / 2 - noiseTime;
+  
+  v = modeFn(angle * wrap, radius * wrap, wrap);
+  v = min(v, 1);
+  paint(v, v * v);
 }
 
 // From ColorWavesWithPalettes by Mark Kriegsman: https://gist.github.com/kriegsman/8281905786e8b2632aeb
@@ -577,5 +549,3 @@ var palettes = [
   BlacK_Red_Magenta_Yellow,
   Blue_Cyan_Yellow,
 ];
-
-var fibonacciToPhysical = [ 0, 39, 19, 58, 29, 9, 48, 20, 59, 38, 10, 49, 28, 1, 40, 18, 57, 30, 8, 47, 21, 60, 37, 11, 50, 27, 2, 41, 17, 56, 31, 7, 46, 22, 61, 36, 12, 51, 26, 3, 42, 16, 55, 32, 6, 45, 23, 62, 35, 13, 52, 25, 4, 43, 15, 54, 33, 5, 44, 24, 63, 34, 14, 53 ]
